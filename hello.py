a20cdbc6ce8902c38c817c381809cda829d132d6
Numan <noumansubhani01@gmail.com>
2025-06-05 11:58:22 +0500

    tweaked 500.html

--- hello.py
+++ hello.py
@@ -42,7 +42,7 @@ def page_not_found(e):
 def page_not_found(e):
         return render_template("404.html"), 404
 
-# Internal Server Error
+# Internal Server Error 
 @app.errorhandler(500)
 def page_not_found(e):
         return render_template("500.html"), 500
